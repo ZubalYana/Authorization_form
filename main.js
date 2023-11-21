b44f1b8208b8ec523158d156c6f90cb0a19e9c20
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2023-11-21 21:39:20 +0200

    add the rest of the functions

--- main.js
+++ main.js
@@ -23,10 +23,28 @@ function bigletter(password) {
         $('#rull_bigLetter').css('color', '#BCA37F');
     }
 }
+function smallLetter(password) {
+    let hassmallletter = /[a-z]/.test(password);
+    if (hassmallletter) {
+        $('#rull_smallLetter').css('color', '#113946');
+    } else {
+        $('#rull_smallLetter').css('color', '#BCA37F');
+    }
+}
+function number(password) {
+    let hasnumber = /\d/.test(password);
+    if (hasnumber) {
+        $('#rull_number').css('color', '#113946');
+    } else {
+        $('#rull_number').css('color', '#BCA37F');
+    }
+}
 
 setInterval(() => {
     let password = $('#AuthForm_password').val();
     checkPassword();
     checkSpecialCase(password); 
     bigletter(password);
+    smallLetter(password);
+    number(password);
 }, 500);
